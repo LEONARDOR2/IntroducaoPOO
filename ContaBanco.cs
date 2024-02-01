40bad52cdd3e841bd306c24c14b39d14298eff95
Leonardo Alves <127170234+LEONARDOR2@users.noreply.github.com>
2024-01-31 23:15:03 -0300

    Adicionar arquivos de projeto.

---
+++ ContaBanco.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MeuSuperBanco;
+
+internal class ContaBanco
+{
+
+    public string Numero { get; }
+    public string Dono { get; set; }
+    public decimal Saldo
+    {
+
+        get
+        {
+            decimal saldo = 0;
+            foreach (var item in todasTransacoes)
+            {
+                saldo += item.Valor;
+            }
+            return saldo;
+        }
+
+
+
+    }
+
+    public static int numeroConta = 123456789;
+
+
+    private List<Transacao> todasTransacoes = new List<Transacao>();
+
+
+    public ContaBanco(string nome, decimal valor)
+    {
+
+        this.Dono = nome;
+
+
+        numeroConta++;
+
+        this.Numero = numeroConta.ToString();
+        Depositar(valor, DateTime.Now, "Saldo inicial");
+    }
+
+
+    public void Depositar(decimal valor, DateTime data, string obs)
+    {
+        if (valor <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(valor), "não aceitamos deposito de valoe 0 ou negativo");
+        }
+        Transacao trans = new Transacao(valor, data, obs);
+        todasTransacoes.Add(trans);
+    }
+
+    public void Sacar(decimal valor, DateTime data, string obs)
+    {
+        if (Saldo - valor <= 0)
+        {
+            throw new InvalidCastException("essa operação não é perminitida");
+        }
+
+
+        if (valor <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(valor), "não aceitamos deposito de valoe 0 ou negativo");
+        }
+
+
+        Transacao trans = new Transacao(-valor, data, obs);
+        todasTransacoes.Add(trans);
+
+    }
+
+    public string PegarMovimentacao()
+    {
+        var movimentacoes = new StringBuilder();
+
+        movimentacoes.AppendLine("Data\t\valor\tObs");
+
+        foreach (var item in todasTransacoes)
+        {
+
+            movimentacoes.AppendLine($"{item.Data.ToShortDateString()}\t {item.Valor}\t {item.Obs}");
+
+        }
+
+        return movimentacoes.ToString();
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+
+namespace MeuSuperBanco;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        MeuSuperBanco.ContaBanco contaB = new ContaBanco("Leonardo", 10000);
+
+        Console.WriteLine($"Aconta {contaB.Numero} de  {contaB.Dono} foi criada com {contaB.Saldo}");
+
+        // contaB.Depositar(0, DateTime.Now, "recebi um pagamneto");
+        // Console.WriteLine($"Aconta está com: {contaB.Saldo} ");
+
+        contaB.Sacar(800, DateTime.Now, "fiz compras");
+        Console.WriteLine($"Aconta está com: {contaB.Saldo} ");
+
+        try
+        {
+            contaB.Sacar(10000, DateTime.Now, "recebi um pagamneto");
+            Console.WriteLine($"Aconta está com: {contaB.Saldo} ");
+
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Operação não realizada");
+        }
+
+
+        contaB.Sacar(1500, DateTime.Now, "Pagar o aluguel");
+        Console.WriteLine($"Aconta está com: {contaB.Saldo} ");
+
+        contaB.Sacar(2000, DateTime.Now, "Pagar a escola das crianças");
+        Console.WriteLine($"Aconta está com: {contaB.Saldo} ");
+
+
+        Console.WriteLine(contaB.PegarMovimentacao());
+
+
+    }
+
+
+
+
+}
